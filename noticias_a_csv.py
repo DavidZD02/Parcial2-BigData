f7852efe20b0ed98f9ce719932045b1458fd4395
DavidZD02 <111716602+DavidZD02@users.noreply.github.com>
2023-04-29 12:44:40 -0500

    Update noticias_a_csv.py

--- noticias_a_csv.py
+++ noticias_a_csv.py
@@ -18,7 +18,7 @@ def elTiempo(file):
     texto = 'titulo,categoria,link\n'
     enlaces_vistos = []
     for articles in soup.find_all('article'):
-        for links in articles.find_all('a',class_='title page-link'):
+        for links in articles.find_all('a', class_='title page-link'):
             try:
                 link = links["href"]
                 if link not in enlaces_vistos:
@@ -26,8 +26,8 @@ def elTiempo(file):
                     categoria = link.split('/')[1]
                     titulo = links.text.strip()
                     texto += f'"{titulo}","{categoria}","{link}"\n'
-            except:
-                ...
+            except KeyError:
+                pass
                 
     url = "headlines/final/periodico=elTiempo/year=" + \
         str(fecha.year)+"/month="+str(fecha.strftime('%m'))+"/elTiempo"
@@ -36,11 +36,11 @@ def elTiempo(file):
 
 
 def elEspectador(file):
-    contenido = s3.get_object(Bucket=bucket, Key=file)["Body"].read() 
+    contenido = s3.get_object(Bucket=bucket, Key=file)["Body"].read()
     soup = BeautifulSoup(contenido, 'html.parser')
-    texto='titulo,categoria,link\n'
+    texto = 'titulo,categoria,link\n'
 
-    for articles in soup.find_all('h2',class_='Card-Title Title Title'):
+    for articles in soup.find_all('h2', class_='Card-Title Title Title'):
       for links in articles.find_all('a'):
         try:
           link='"'+links["href"]+'"'
