61498bf2179b798aa8a4f8096c28c9763fb3dcaa
DavidZD02 <111716602+DavidZD02@users.noreply.github.com>
2023-04-29 14:42:26 -0500

    Update descargarNoticias.py

--- descargarNoticias.py
+++ descargarNoticias.py
@@ -2,14 +2,17 @@ import urllib.request
 import datetime
 import boto3
 
-def descargar(url,nombre):
-    bucket='noticias-parcial99'
+
+def descargar(url, nombre):
+    bucket = 'noticias-parcial99'
     response = urllib.request.urlopen(url)
     contenido = response.read().decode('utf-8')
     fecha = datetime.date.today().strftime('%Y-%m-%d')
     s3 = boto3.resource('s3')
     s3.Object(bucket, 'headlines/raw/{}{}.html'.format(
         nombre, fecha)).put(Body=contenido)
-        
+    return contenido
+
+
 descargar('https://www.eltiempo.com', 'elTiempo-')
 descargar('https://www.elespectador.com',  'elEspectador-')
